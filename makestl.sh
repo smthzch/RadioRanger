934ebcd09df5a2c2dfc26e9af47b301d46022812
smthzch <ezke.smith@gmail.com>
2025-12-21 12:05:04 -0500

    updaate scad location

--- makestl.sh
+++ makestl.sh
@@ -1,2 +1,2 @@
-openscad -D 'section="top"' -o stl/radioranger_top.stl scad/radioranger.scad
-openscad -D 'section="bottom"' -o stl/radioranger_bottom.stl scad/radioranger.scad
+openscad -D 'section="top"' -o scad/radioranger_top.stl scad/radioranger.scad
+openscad -D 'section="bottom"' -o scad/radioranger_bottom.stl scad/radioranger.scad
