f82b02a60f6d6b9e79eaece6c618e10722d0b995
smthzch <ezke.smith@gmail.com>
2025-12-24 00:17:57 -0500

    pin layount mono radio

--- src/mono_radio/main.cpp
+++ src/mono_radio/main.cpp
@@ -8,13 +8,15 @@ the relative strength.
 ===========================================================
             Pin Layout
 
-BATT(+/-)-|A0  USB  5V|-
-         -|D1      GND|-LED(G)
-         -|D2      3V3|-LED(VCC)
-         -|D3      D10|-
-LED(SDA) -|D4       D9|-
-LED(SCL) -|D5       D8|-
-         -|D6       D7|-
+        -|A0  USB  5V|-
+        -|D1      GND|-
+        -|D2      3V3|-
+        -|D3      D10|-
+        -|D4       D9|-
+        -|D5       D8|-
+        -|D6       D7|-
+
+There is no pin layout, connect a battery.
 */
 
 #include <Arduino.h>
